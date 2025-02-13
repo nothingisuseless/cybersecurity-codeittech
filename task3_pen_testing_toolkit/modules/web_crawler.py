4a86fa0b85d1f114ed5dace90f0568de2dea74ed
nothingisuseless <devopriyasearchingjobs@gmail.com>
2025-02-13 23:26:28 +0530

    Update web_crawler.py

--- task3_pen_testing_toolkit/modules/web_crawler.py
+++ task3_pen_testing_toolkit/modules/web_crawler.py
@@ -1,17 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl(url):
+def crawl(url,st):
     response = requests.get(url)
     if response.status_code == 200:
-        print(f"Successfully connected to {url}")
+        st.write(f"Successfully connected to {url}")
         soup = BeautifulSoup(response.text, 'html.parser')
         links = soup.find_all('a', href=True)
         for link in links:
-            print(f"Found link: {link['href']}")
+            st.write(f"Found link: {link['href']}")
     else:
-        print(f"Failed to connect to {url}")
-
-if __name__ == '__main__':
-    target_url = input("Enter target URL: ")
-    crawl(target_url)
+        st.write(f"Failed to connect to {url}")
