0263aa5df9052a96ac4f8b9b4887ffd456052126
nothingisuseless <devopriyasearchingjobs@gmail.com>
2025-02-13 23:26:07 +0530

    Update port_scanner.py

--- task3_pen_testing_toolkit/modules/port_scanner.py
+++ task3_pen_testing_toolkit/modules/port_scanner.py
@@ -1,22 +1,21 @@
 import socket
 from concurrent.futures import ThreadPoolExecutor
 
-def scan_port(target, port):
+
+def scan_port(target, port,lis):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
         result = sock.connect_ex((target, port))
         if result == 0:
-            print(f"Port {port} is open on {target}")
+            lis.append(f"Port {port} is open on {target}")
         sock.close()
     except Exception as e:
         pass
 
 def scan_target(target, ports):
+    lis=[]
     with ThreadPoolExecutor(max_workers=20) as executor:
-        executor.map(lambda port: scan_port(target, port), ports)
-
-if __name__ == '__main__':
-    target_ip = input("Enter target IP address: ")
-    ports = range(1, 65535)
-    scan_target(target_ip, ports)
+        executor.map(lambda port: scan_port(target, port,lis), ports)
+        return lis
+        
